d11932989d3dd317338576f5a2a0eb52110e9cbb
= <=>
2021-08-05 12:02:10 -0400

    adding code

---
+++ ProyectAboutAnimal/ProyectAboutAnimal/ProyectAboutAnimal/Const.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectAboutAnimal
+{
+    public static class Const
+    {
+        public static string Navigation = "Nav";
+        public static string MainPage = "Main";
+
+
+        public static string urlfactFogs = @"https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all";
+        public static string urlfactDogRandom = @"https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?index=0";
+    }
+}
---
+++ ProyectAboutAnimal/ProyectAboutAnimal/ProyectAboutAnimal/Services/IfactDogsService.cs
@@ -0,0 +1,13 @@
+﻿using ProyectAboutAnimal.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectAboutAnimal.Services
+{
+    public interface IfactDogsService
+    {
+        Task<FactDogs> GetFactDogs();
+    }
+}
